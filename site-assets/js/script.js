4f3f8f05994386528e17481a45d1191fb2dd94b8
Carlos Santos <carlos@one-shift.com>
2018-05-13 23:04:26 +0100

    commenting code
    I just don't know it is there

--- site-assets/js/script.js
+++ site-assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-    $('#fullpage').fullpage({
-        sectionsColor: ['#177679']
-    });
+	// $('#fullpage').fullpage({
+	// 	sectionsColor: ['#177679']
+	// });
 
 });
